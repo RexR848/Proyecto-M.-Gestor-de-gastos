0c13d5e47aef4428985c740a15e26e73356d4961
Jose <armand.jose117@gmail.com>
2025-08-02 17:26:08 -0600

    Colocando el codigo para poder abrir la navbar y poder cerrar la sesión porque lo quite en el commit pasado

--- public/Reporte.js
+++ public/Reporte.js
@@ -193,3 +193,42 @@ document.addEventListener("DOMContentLoaded", async () => {
     console.error("Error cargando datos destacados y gráfica:", error);
   }
 });
+
+//navbar
+function toggleSidebar() {
+  document.getElementById("sidebar").classList.toggle("open");
+}
+
+//Logica para cerrar sesión
+const logoutLink = document.getElementById("logout-link");
+const overlay = document.getElementById("overlay");
+const popup = document.getElementById("logout-popup");
+const cancelBtn = document.querySelector(".cancel-btn");
+const confirmBtn = document.querySelector(".confirm-btn");
+
+logoutLink.addEventListener("click", function(e) {
+  e.preventDefault();
+  popup.classList.add("active");
+  overlay.classList.add("active");
+});
+
+cancelBtn.addEventListener("click", () => {
+  popup.classList.remove("active");
+  overlay.classList.remove("active");
+});
+
+confirmBtn.addEventListener("click", () => {
+  fetch('/logout', {
+    method: 'POST',
+    credentials: 'include'
+  })
+  .then(res => res.json())
+  .then(data => {
+    if (data.ok) {
+      window.location.href = '../index.html';
+    } else {
+      alert('No se pudo cerrar sesión.');
+    }
+  })
+  .catch(() => alert('Error en la comunicación con el servidor.'));
+});
