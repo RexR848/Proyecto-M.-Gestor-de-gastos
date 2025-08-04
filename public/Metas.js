e9587d107fabf8568cee15cb3ef4f22658166bc6
Alan P. <alanperaltau88@gmail.com>
2025-08-04 02:23:12 -0600

    function mostrar metas existentes

--- public/Metas.js
+++ public/Metas.js
@@ -15,5 +15,29 @@ document.addEventListener("DOMContentLoaded", () => {
   window.toggleSidebar = function () {
     document.getElementById("sidebar").classList.toggle("open");
   };
+  function mostrarMetas() {
+    metasContainer.innerHTML = "";
+
+    metas.forEach((m, i) => {
+      const div = document.createElement("div");
+      div.className = "gasto-box";
+
+      const porcentaje = Math.min((m.actual / m.meta) * 100, 100).toFixed(1);
+
+      div.innerHTML = `
+        <div class="gasto-header">${m.nombre}</div>
+        <p style="margin:8px 0;">💵 ${m.actual.toFixed(2)} / ${m.meta.toFixed(2)}</p>
+        <div style="background:#444; border-radius:8px; overflow:hidden; margin: 8px 0;">
+          <div style="width:${porcentaje}%; height:12px; background:#4aa3ff;"></div>
+        </div>
+        <div style="margin-top:10px;">
+          <button class="btn" onclick="abrirPopup(${i}, 'ingreso')">+ Ingresar</button>
+          <button class="btn" onclick="abrirPopup(${i}, 'retiro')">- Retirar</button>
+        </div>
+      `;
+      metasContainer.appendChild(div);
+    });
+  }
+
 
 });
