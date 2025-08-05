495d47f55cdf3d9e6655309264e7167df5994351
Alan P. <alanperaltau88@gmail.com>
2025-08-04 19:18:55 -0600

    Update Metas.js

--- public/js/Metas.js
+++ public/js/Metas.js
@@ -100,7 +100,6 @@ document.addEventListener("DOMContentLoaded", () => {
     await guardarMetas();
     popup.classList.remove("active");
     overlay.classList.remove("active");
-    mostrarMetas();
     location.reload();
   };
 
@@ -131,40 +130,3 @@ document.addEventListener("DOMContentLoaded", () => {
   //nv meta btn
   nuevaBtn.addEventListener("click", abrirPopupNuevaMeta);
 });
-
-// Manejo de cierre de sesión
-document.addEventListener("DOMContentLoaded", () => {
-  const logoutLink = document.getElementById("logout-link");
-  const overlay = document.getElementById("overlay");
-  const popup = document.getElementById("logout-popup");
-  const cancelBtn = document.querySelector(".cancel-btn");
-  const confirmBtn = document.querySelector(".confirm-btn");
-
-  logoutLink.addEventListener("click", function (e) {
-    e.preventDefault();
-    popup.classList.add("active");
-    overlay.classList.add("active");
-  });
-
-  cancelBtn.addEventListener("click", () => {
-    popup.classList.remove("active");
-    overlay.classList.remove("active");
-  });
-
-  confirmBtn.addEventListener("click", () => {
-    fetch("/logout", {
-      method: "POST",
-      credentials: "include",
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.ok) {
-          window.location.href = "../index.html";
-        } else {
-          alert("No se pudo cerrar sesión.");
-        }
-      })
-      .catch(() => alert("Error en la comunicación con el servidor."));
-  });
-});
-
